b7377e3cbb6ae8be4221d10a3651d9a17a72166a
EyalIlan <eyalilan8@gmail.com>
2022-02-17 16:07:56 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <div className="App container">
         <h1>hello world</h1>
         <h1>Main Title</h1>
+        
         <CreatePost></CreatePost>
         <hr />
         <PostList></PostList>
