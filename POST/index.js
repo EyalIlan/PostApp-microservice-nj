9369d6d3a2c2d54a4a4fd0231ca3c8ee50aaad80
EyalIlan <eyalilan8@gmail.com>
2022-02-17 14:50:30 +0200

    front post & comment

--- POST/index.js
+++ POST/index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const {randomBytes} = require('crypto')
 const app = express()
 const body = require('body-parser')
+const cors = require('cors')
+app.use(cors())
+
 
 app.use(body.urlencoded({extended:true}))
 app.use(body.json())
@@ -9,7 +12,6 @@ app.use(body.json())
 const posts = {};
 
 app.get('/posts',(req,res) =>{
-
     res.send(posts)
 
 })
@@ -17,8 +19,7 @@ app.get('/posts',(req,res) =>{
 app.post('/posts',(req,res) =>{
 
     const id = randomBytes(4).toString('hex')
-    const {title} = req.body
-
+    const title = req.body.post
     posts[id] = {
         id,title
     }
@@ -26,7 +27,7 @@ app.post('/posts',(req,res) =>{
 
 })
 
-
+console.log(posts);
 
 app.listen(5000,(req,res) =>{
   
